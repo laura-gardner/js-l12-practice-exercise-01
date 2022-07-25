023d7926a60fb429f44d1679a95f27458b7810b4
laura-gardner <elgardner@gmail.com>
2022-07-25 11:54:37 +0100

    Added functionality to allow 1, 3 or 5 random users to be displayed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserElement = document.querySelector("#users");
 
-const getData = async function () {
-    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function (numUsers) {
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();
     const userResults = data.results;
     displayUsers(userResults);
@@ -23,4 +24,10 @@ const displayUsers = function (userResults) {
     }
 }
 
-getData();
+
+selectUserElement.addEventListener("change", function () {
+    const numUsers = selectUserElement.value;
+    getData(numUsers);
+})
+
+getData(1);
